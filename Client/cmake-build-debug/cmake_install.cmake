6edb1bf9b3599db758ce3d91025384a343621dff
didhat <didhat82@gmail.com>
2022-05-28 12:37:46 +0300

    -

--- Client/cmake-build-debug/cmake_install.cmake
+++ Client/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/daniil/CLionProjects/TestClient
+# Install script for directory: /home/daniil/MessangerHSE/Client
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/daniil/CLionProjects/TestClient/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/daniil/MessangerHSE/Client/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
